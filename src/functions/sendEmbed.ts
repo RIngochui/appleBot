6d539a08fd07225e6eb4a775461c3ced9e52bc42
RIngochui <ringochewyis@gmail.com>
2022-11-03 14:05:27 -0400

    front end changes for order of text displayed

--- src/functions/sendEmbed.ts
+++ src/functions/sendEmbed.ts
@@ -4,10 +4,10 @@ import { settings } from "../bot";
 export const sendEmbed = async (client: Client, string: String, url: string, title: string, thumbnail?: string) => {
     const channel = await client.channels.fetch(settings.ID).catch(console.error);
     if (channel instanceof TextChannel) {
+        (channel as TextChannel).send({ content: `${string}` });
         const exampleEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
         .setURL(url)
-        .setDescription(`${string}`)
         .setTitle(title)
         if (thumbnail)
             exampleEmbed.setThumbnail(thumbnail)
